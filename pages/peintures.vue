b156a68bfba6c02cbf81f2fc0600a22c2e30caa1
Kevin Rumeur <lesourdingo@gmail.com>
2024-07-03 15:38:45 +0200

    chore: Add Vue Masonry Wall for responsive image grid layout in peintures.vue

--- pages/peintures.vue
+++ pages/peintures.vue
@@ -25,11 +25,17 @@
   </div>
   <div class="p-4">
     <ContentList path="/peintures" v-slot="{ list }">
-      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-        <div v-for="item in list" :key="item._path">
-          <ContentRenderer :value="item" />
-        </div>
-      </div>
+      <MasonryWall :items="list" :ssr-columns="1" :column-width="330" :gap="16">
+        <template #default="{ item }">
+          <div class="masonry-item">
+            <ContentRenderer :value="item" />
+          </div>
+        </template>
+      </MasonryWall>
     </ContentList>
   </div>
 </template>
+
+<script lang="ts" setup>
+import MasonryWall from "@yeger/vue-masonry-wall";
+</script>
