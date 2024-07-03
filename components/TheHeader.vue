5fe2a33a1fe760facd8dffe7cedb4fedb83e0ac3
Kevin Rumeur <lesourdingo@gmail.com>
2024-07-03 19:24:58 +0200

    add scultures

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="navbar bg-base-100">
     <div class="navbar-start">
-      <NuxtLink to="/" class="btn btn-ghost text-2xl font-bold">
+      <NuxtLink
+        to="/"
+        class="btn btn-ghost text-2xl font-semibold md:font-bold"
+      >
         Brigitte Rouxel
       </NuxtLink>
     </div>
-    <div class="navbar-center">
+    <div class="navbar-center hidden md:block">
       <ul class="menu menu-horizontal px-1 text-xl">
         <li>
           <NuxtLink to="/peintures" exact-active-class="active"
@@ -21,6 +24,22 @@
     </div>
     <div class="navbar-end">
       <NuxtLink class="btn">Contact</NuxtLink>
+    </div>
+  </div>
+  <div class="flex justify-center md:hidden">
+    <div class="navbar-center">
+      <ul class="menu menu-horizontal px-1 text-xl">
+        <li>
+          <NuxtLink to="/peintures" exact-active-class="active"
+            >Peintures</NuxtLink
+          >
+        </li>
+        <li>
+          <NuxtLink to="/scultures" exact-active-class="active"
+            >Scultures</NuxtLink
+          >
+        </li>
+      </ul>
     </div>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,22 @@
 <template>
   <TheHeader />
-  <div class="p-4">
-    <ContentList path="/peintures" v-slot="{ list }">
-      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-        <div v-for="item in list" :key="item._path">
-          <ContentRenderer :value="item" />
-        </div>
+  <div class="flex flex-col md:flex-row gap-8 justify-center md:mt-8">
+    <NuxtLink to="/peintures" class="card bg-base-100 w-96 shadow-xl p-4">
+      <figure class="w-80 md:h-64">
+        <img src="/img/peintures/photo1.jpg" height="250" alt="peinture" />
+      </figure>
+      <div class="card-body">
+        <h2 class="card-title">Toutes les peintures</h2>
       </div>
-    </ContentList>
+    </NuxtLink>
+    <NuxtLink to="/scultures" class="card bg-base-100 w-96 shadow-xl p-4">
+      <figure class="w-80 md:h-64">
+        <img src="/img/scultures/sculture11.jpg" height="250" alt="Shoes" />
+      </figure>
+      <div class="card-body">
+        <h2 class="card-title">Toutes les scultures</h2>
+      </div>
+    </NuxtLink>
   </div>
 </template>
 
