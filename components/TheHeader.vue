02b0df8f02f013f04c06dd709795f38830e9bb41
Kevin Rumeur <lesourdingo@gmail.com>
2024-07-03 17:48:56 +0200

    add paints

---
+++ components/TheHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="navbar bg-base-100">
+    <div class="navbar-start">
+      <NuxtLink to="/" class="btn btn-ghost text-2xl font-bold">
+        Brigitte Rouxel
+      </NuxtLink>
+    </div>
+    <div class="navbar-center">
+      <ul class="menu menu-horizontal px-1 text-xl">
+        <li>
+          <NuxtLink to="/peintures" exact-active-class="active"
+            >Peintures</NuxtLink
+          >
+        </li>
+        <li>
+          <NuxtLink to="/scultures" exact-active-class="active"
+            >Scultures</NuxtLink
+          >
+        </li>
+      </ul>
+    </div>
+    <div class="navbar-end">
+      <NuxtLink class="btn">Contact</NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,10 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxt/content", "@nuxtjs/tailwindcss", "@nuxt/image"],
   routeRules: {
-    "/**": { prerender: true },
+    // Cache static assets for 1 hour
+    "/assets/**": { headers: { "cache-control": "max-age=3600" } },
+    // Cache pre-rendered pages for 1 hour
+    "/**": { swr: 3600 },
   },
   image: {
     quality: 20,
