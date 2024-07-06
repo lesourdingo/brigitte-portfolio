9003d199762d8fdcf380a20615b26020e991aed7
Kevin Rumeur <lesourdingo@gmail.com>
2024-07-06 18:44:39 +0200

    fix: padding

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,25 @@
 <template>
   <TheHeader />
-  <div class="flex flex-col md:flex-row gap-8 justify-center md:mt-8">
-    <NuxtLink to="/peintures" class="card bg-base-100 w-96 shadow-xl p-4">
-      <figure class="w-80 md:h-64">
-        <img src="/img/peintures/photo1.jpg" height="250" alt="peinture" />
+  <div class="flex flex-col md:flex-row gap-8 justify-center md:mt-8 p-4">
+    <NuxtLink to="/peintures" class="card bg-primary w-96 shadow-xl p-4">
+      <figure class="w-full h-64 flex items-center justify-center">
+        <NuxtImg
+          src="/img/peintures/photo1.jpg"
+          class="max-w-full max-h-full object-contain"
+          alt="peinture"
+        />
       </figure>
       <div class="card-body">
         <h2 class="card-title">Toutes les peintures</h2>
       </div>
     </NuxtLink>
-    <NuxtLink to="/scultures" class="card bg-base-100 w-96 shadow-xl p-4">
-      <figure class="w-80 md:h-64">
-        <img src="/img/scultures/sculture11.jpg" height="250" alt="Shoes" />
+    <NuxtLink to="/scultures" class="card bg-secondary w-96 shadow-xl p-4">
+      <figure class="w-full h-64 flex items-center justify-center">
+        <NuxtImg
+          src="/img/scultures/sculture11.jpg"
+          class="max-w-full max-h-full object-contain"
+          alt="Shoes"
+        />
       </figure>
       <div class="card-body">
         <h2 class="card-title">Toutes les scultures</h2>
